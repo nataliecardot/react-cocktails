a64b3141e671d785ca4d288fd4623dfc44d10850
nataliecardot <nataliecardot@gmail.com>
2021-10-05 20:22:31 -0700

    Remove junk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../logo.svg'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
       <h2>navbar component</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
