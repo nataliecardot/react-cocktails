fb82cff0103e6befbaec4f98b50dc33c073b0174
nataliecardot <nataliecardot@gmail.com>
2021-10-05 21:59:33 -0700

    Add about page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse omnis fuga
+        iste obcaecati ratione rerum consectetur soluta temporibus voluptates,
+        maxime nihil officia sunt atque? Accusantium magni sapiente assumenda
+        sed labore perspiciatis, iure facilis consequatur explicabo porro
+        repudiandae voluptate dolor animi.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
