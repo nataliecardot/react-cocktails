a68ed6fd5035901b1a5d2732cb7eb30e7617277e
nataliecardot <nataliecardot@gmail.com>
2021-10-05 21:53:46 -0700

    Remove extra div

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,23 +4,21 @@ import logo from '../logo.svg';
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="navbar">
-        <div className="nav-center">
-          <Link to="/">
-            <img src={logo} alt="cocktail db logo" className="logo" />
-          </Link>
-        </div>
-        <ul className="nav-links">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <nav className="navbar">
+      <div className="nav-center">
+        <Link to="/">
+          <img src={logo} alt="cocktail db logo" className="logo" />
+        </Link>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
