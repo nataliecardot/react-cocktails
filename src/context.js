70a49f33240fe379aa5f68f66d13e5a26e0ab484
nataliecardot <nataliecardot@gmail.com>
2021-10-21 21:51:51 -0700

    Fetch cocktails

--- src/context.js
+++ src/context.js
@@ -6,9 +6,46 @@ const AppContext = React.createContext();
 
 // children prop is automatically passed by React; it represents the content between the opening and the closing tags when invoking/rendering a component https://stackoverflow.com/a/62790515/8888320
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState('a');
   const [cocktails, setCocktails] = useState([]);
+
+  // called each time input changes
+  // gives object with a drinks property which has an array as a value
+  const fetchDrinks = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      // despite the method being named json(), the result is not JSON but is instead the result of taking JSON as input and parsing it to produce a JavaScript object. "Parse as JSON" means analyze it in JSON format - but JS is returned
+      const data = await response.json();
+      const { drinks } = data;
+      if (drinks) {
+        // transform to easier-to-manage property names
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm]);
 
   return (
     <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
