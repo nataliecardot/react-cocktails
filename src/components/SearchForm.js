0e9e3a53bf4d094eb1c7c8d595afa2f1af57b21f
nataliecardot <nataliecardot@gmail.com>
2021-11-11 21:46:33 -0800

    Add search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,10 +4,38 @@ import { useGlobalContext } from '../context';
 // this is uncontrolled component (see https://stackoverflow.com/a/42522792/8888320)
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  // corresponds to ref defined on input element
+  const searchValue = React.useRef('');
+
+  // removed because can just use autoFocus attribute instead
+  // React.useEffect(() => {
+  //   searchValue.current.focus();
+  // }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    // prevent app refresh when user hits enter
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Search for your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            autoFocus
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
